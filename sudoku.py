9dc4b4dee89e0a73cdb93ad72b71376581cd12fb
Elena <75449340+DeianaElena@users.noreply.github.com>
2021-11-09 12:23:20 +0100

    #function to read dimacs files

--- sudoku.py
+++ sudoku.py
@@ -6,6 +6,20 @@ class Sudoku:
         self.base_rules = self.get_base_rules()
 
     def get_base_rules(self) -> List:
+        
+        #function to read dimacs files
+        def read_dimacs(self) -> List:
+            my_txt='sudoku-example.txt'
+            with open(my_txt) as s:
+                my_d=s.readlines()
+            
+            sudo_list=[]    
+            for row in my_d:
+                row = row.rstrip(" 0\n")
+                row = row.split(" ")
+                sudo_list.append(row)
+            return sudo_list
+        
         with open("sudoku-rules.txt") as f:
             rules = f.readlines()
 
