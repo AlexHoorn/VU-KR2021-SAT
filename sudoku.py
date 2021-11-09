b3410f53526e7da20a2788942d95a5599448a7ff
Alex Hoorn <_alex_@hotmail.nl>
2021-11-09 20:00:43 +0100

    Adding some commenting

--- sudoku.py
+++ sudoku.py
@@ -1,49 +1,85 @@
-from typing import List
+from typing import Generator, List, Tuple
 import numpy as np
 
 
 class Sudoku:
-    def __init__(self, filename: str) -> None:
+    def __init__(self, filepath: str) -> None:
+        """Implements the Sudoku puzzle.
+
+        Args:
+            filepath (str): string of the filepath of Sudoku in DIMACS.
+        """
         self.base_rules = self.get_base_rules()
-        self.constraints = read_dimacs(filename=filename)
+        self.constraints = read_dimacs(filepath=filepath)
 
         self.answers: List[int] = []
 
-    def get_base_rules(self, filename="sudoku-rules.txt") -> List:
-        return read_dimacs(filename=filename)
+    def __iter__(self) -> Generator[List]:
+        """Iterator that makes iterating over all clauses easy with e.g. `for clause in sudoku`.
 
-    def add_answer(self, clause):
+        Yields:
+            Generator[List]: every clause in rules + constraints + answers.
+        """
+        clauses = self.get_all_clauses()
+        for c in clauses:
+            yield c
+
+    def get_base_rules(self) -> List:
+        """Simply wraps read_dimacs with the path to the rules."""
+        return read_dimacs(filepath="sudoku-rules.txt")
+
+    def add_answer(self, clause: int) -> None:
+        """Add answer to puzzle.
+
+        Args:
+            clause (int): int representing answer i.e. 135 row 1, column 3 and value 5.
+        """
         if clause not in self.constraints:
             self.answers.append(clause)
 
-    def clear_answers(self):
+    def clear_answers(self) -> None:
+        """Empty list of answers."""
         self.answers = []
 
-    def get_all_clauses(self):
+    def get_all_clauses(self) -> List:
+        """Return all clauses in the puzzle (rules + constraints + answers)."""
         return self.base_rules + self.constraints + self.answers
 
-    def check_satisfied(self):
-        rules, negatives = self._get_map(self.base_rules)
-        clauses_boolean = list(map(self._map_value, rules, negatives))
+    def check_satisfied(self) -> bool:
+        """Check if puzzle is satisfied with the given answers.
+
+        Returns:
+            bool: boolean stating True if satisfied.
+        """
+        # Split values and whether they are negated
+        rules, negations = self._get_map(self.base_rules)
+
+        clauses_boolean = list(map(self._map_value, rules, negations))
         clauses_boolean = list(map(np.any, clauses_boolean))
 
         return np.all(clauses_boolean)
 
-    def _get_map(self, clauses):
+    def _get_map(self, clauses) -> Tuple[List, List]:
+        """Split clauses into absolutes of variables and a mask whether they are negated"""
+        # TODO: This can be much faster
+
+        # Convert clauses to absolute values
         values = [[abs(v) for v in c] for c in clauses]
-        negatives = [[v < 0 for v in c] for c in clauses]
+        # Determine which values have to be negated
+        negations = [[v < 0 for v in c] for c in clauses]
 
-        return values, negatives
+        return values, negations
 
-    def _map_value(self, values, negatives):
+    def _map_value(self, values, negatives) -> List:
+        """"""
         clause = np.isin(values, self.constraints + self.answers)
-        clause = np.where(negatives, clause, ~clause)
+        clause = np.where(negatives, ~clause, clause)
 
         return clause
 
 
-def read_dimacs(filename: str) -> List:
-    with open(filename) as f:
+def read_dimacs(filepath: str) -> List:
+    with open(filepath) as f:
         dimacs_lines = f.read().splitlines()
 
     clause_list = []
