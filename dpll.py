ea20324423afe3cc4e3d64a8b3de7e2d8faeb05f
Elena <75449340+DeianaElena@users.noreply.github.com>
2021-11-15 16:36:23 +0100

    def check_unit_clause

--- dpll.py
+++ dpll.py
@@ -1,15 +1,23 @@
-#pseudocode, not sure how to integrade it with the rest of the code yet
+###pseudocode
 
-def dpll(X, literal):
-    remove clauses from X containing literal
-    shorten clauses from X containing not literal
-    if X contains no clauses:
-        return True;
-    if X contains empty clause:
-        return False;
+# def dpll(X, literal):
+#     remove clauses from X containing literal
+#     shorten clauses from X containing not literal
+#     if X contains no clauses:
+#         return True;
+#     if X contains empty clause:
+#         return False;
 
-    choose P in X:
-    if (dpll(X , not P)):
-        return True;
+#     choose P in X:
+#     if (dpll(X , not P)):
+#         return True;
   
-    return dpll(X, P)
+#     return dpll(X, P)
+#----------------------------------------------------------------------------
+
+#Checking if there is a unit clause (only one literal clause) 
+def check_unit_clause(cnf):
+    for clause in cnf:
+        if len(clause) == 1:
+            return True
+        return False
